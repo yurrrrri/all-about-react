b0adef05df1f2b54d1e5ef39b1ec02885453af8e
yurison <yu_ri_son@naver.com>
2025-08-16 07:05:02 +0900

    feat: 검색 input 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,57 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+
+const Nav = () => {
+  const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <NavWrapper show={show}>
+      <Logo>
+        <img
+          alt="Disney Plus Logo"
+          src="/images/logo.svg"
+          onClick={() => (window.location.href = "/")}
+        />
+      </Logo>
+
+      {pathname === "/" ? (
+        <Login>Login</Login>
+      ) : (
+        <Input
+          value={searchValue}
+          onChange={handleChange}
+          className="nav__input"
+          type="text"
+          placeholder="영화를 검색해 주세요."
+        />
+      )}
+    </NavWrapper>
+  );
+};
+
+export default Nav;
 
 const NavWrapper = styled.nav`
   position: fixed;
@@ -29,18 +82,28 @@ const Logo = styled.a`
   }
 `;
 
-const Nav = () => {
-  return (
-    <NavWrapper>
-      <Logo>
-        <img
-          alt="Disnet Plus logo"
-          src="/images/logo.svg"
-          onClick={() => (window.location.href = "/")}
-        />
-      </Logo>
-    </NavWrapper>
-  );
-};
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
 
-export default Nav;
+  &:hover {
+    background-color: #f9f9f9;
+    color: gray;
+    border-color: transparent;
+  }
+`;
+
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.582);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: none;
+`;
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,7 +1,71 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
+import "./SearchPage.css";
 
 const SearchPage = () => {
-  return <div></div>;
+  const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  const fetchSearchMovie = async () => {
+    try {
+      const response = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(response.data.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  if (searchResults.length > 0) {
+    return (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.meida_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  className="movie__column-poster"
+                  onClick={() => navigate(`/${movie.id}`)}
+                >
+                  <img
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    );
+  } else {
+    return (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾고자 하는 검색어 "{searchTerm}"에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    );
+  }
 };
 
 export default SearchPage;
