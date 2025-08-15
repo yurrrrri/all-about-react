2565037fb029c94542fd0f9ebd28c2eeca7dd1c0
yurison <yu_ri_son@naver.com>
2025-08-16 00:15:31 +0900

    feat: React-router-dom 적용

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
-import styled from "styled-components";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
-import Banner from "./components/Banner";
-import Category from "./components/Category";
-import Row from "./components/Row";
-import requests from "./api/requests";
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <Container>
-      <Nav />
-      <Banner />
-      <Category />
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row
-        title="Comedy Movies"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-    </Container>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="main" element={<MainPage />} />
+          <Route path=":movieId" element={<MainPage />} />
+          <Route path="search" element={<MainPage />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
 export default App;
-
-const Container = styled.main`
-  position: relative;
-  min-height: calc(100vh - 250px);
-  overflow-x: hidden;
-  display: block;
-  top: 72px;
-  padding: 0 calc(3.5vw + 5px);
-
-  &:after {
-  background: url("/images/home-background.png") center center / cover no-repeat fixed;
-  content: "";
-  position: absolute;
-  inset: 0px;
-  opacity: 1;
-  z-index: -1;
-`;
